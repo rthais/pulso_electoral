90965d83a942a43838c9633286ab79271905e068
Roberto Thais <roberto.n.thais@gmail.com>
2011-03-21 04:06:26 -0400

    mapreduce jobs

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-require.paths.unshift(__dirname + '/lib')
+require.paths.unshift(__dirname + '/lib');
 
-var twitter   = require('twitter');
-var filters   = require('filters');
-var processor = require('processor')
-var database  = require('database')
-var server    = require('server')
+var twitter   = require('twitter'),
+    filters   = require('filters'),
+    processor = require('processor'),
+    database  = require('database'),
+    server    = require('server')
 
 // Call only once
 twitter.start(filters.KEYWORDS, function(tweet){
-    var tally = processor.process(filters.MAP, tweet.text)
-    database.persist(tweet, tally)
-    server.broadcast(tally)
+    var tally = processor.process(filters.MAP, tweet.text);
+    database.persist(tweet, tally);
+    server.broadcast(tally);
 });
 
-server.start()
+server.start();
+
+setInterval(function(){
+    database.Tweet.getScores(function(result){
+        server.broadcast(result)
+    })
+}, 500)
