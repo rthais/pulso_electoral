72e9b017b1ce29075da3ce042534167dd20f88d9
Roberto Thais <roberto.n.thais@gmail.com>
2011-03-29 14:50:33 -0400

    store credentials as json

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var environment = process.env.NODE_ENV || 'development'
+
 var config = {
     calculationInterval: 500,
     mock: {
@@ -6,5 +9,9 @@ var config = {
       weight: 0.9
     },
 }
+
+// Synchronous, since we only do it once
+var credentials = JSON.parse(fs.readFileSync('credentials', 'utf8'))[environment]
+config.credentials = credentials
 
 exports.Config = config
